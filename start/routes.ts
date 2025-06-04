5669f57aba2583759774125caccb1d8eb347a7e3
JESSICA6-ZABIBU <jesszabibu15@gmail.com>
2025-06-04 13:22:38 +0200

    creation de la page creer un compte et styliser

--- start/routes.ts
+++ start/routes.ts
@@ -8,7 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
-router.on('/').render('twitter/home')
+router.on('/').render('twitter/inscris')
 // router.get('/home','#controllers/twitter_controller.homes')
 // router.get('/profil','#controllers/twitter_controller.profil')
 
